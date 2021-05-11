622c9f5bad7b17676a655deafc5bed91630b3ec0
aarati-K <aarati.agc10@gmail.com>
2021-05-10 20:42:20 -0500

    Scripts for running workloads

---
+++ run_zipf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+make
+for z in {0,0.5,1,1,5,2,2.5,3,3.5,4}:
+do
+    echo workloads/zipf_${z}_chained_default_1B
+    taskset -c 20 ./benchmark.out $filename workloads/zipf_${z}_chained_default_1B
+    echo "--------"
+    sleep 10
+
+    echo workloads/zipf_${z}_chained_sorted_1B
+    taskset -c 20 ./benchmark.out $filename workloads/zipf_${z}_chained_sorted_1B
+    echo "--------"
+done
