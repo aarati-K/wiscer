04016c7bccc9e6d78c98642570eae2f9b51f5804
aarati-K <aarati.agc10@gmail.com>
2021-05-11 16:37:56 -0500

    Add zipf 4.5 and 5, sleep for lesser duration

--- run_zipf.sh
+++ run_zipf.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 make
-for z in {0,0.5,1,1.5,2,2.5,3,3.5,4}
+for z in {0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5}
 do
     echo workloads/zipf_${z}_chained_default_100M
     taskset -c 39 ./benchmark.out workloads/zipf_${z}_chained_default_100M
     echo "--------"
-    sleep 30
+    sleep 10
 
     echo workloads/zipf_${z}_chained_sorted_100M
     taskset -c 39 ./benchmark.out workloads/zipf_${z}_chained_sorted_100M
     echo "--------"
-    sleep 30
+    sleep 10
 done
