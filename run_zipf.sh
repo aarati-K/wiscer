7cf5df4d87df0e5d86be4f364fa1de76b1c87460
aarati-K <aarati.agc10@gmail.com>
2021-05-11 12:38:23 -0500

    Fix bug in script

--- run_zipf.sh
+++ run_zipf.sh
@@ -3,12 +3,12 @@
 make
 for z in {0,0.5,1,1.5,2,2.5,3,3.5,4}
 do
-    echo workloads/zipf_${z}_chained_default_1B
+    echo workloads/zipf_${z}_chained_default_100M
     taskset -c 39 ./benchmark.out workloads/zipf_${z}_chained_default_1B
     echo "--------"
     sleep 30
 
-    echo workloads/zipf_${z}_chained_sorted_1B
+    echo workloads/zipf_${z}_chained_sorted_100M
     taskset -c 39 ./benchmark.out workloads/zipf_${z}_chained_sorted_1B
     echo "--------"
     sleep 30
