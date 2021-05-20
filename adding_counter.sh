406647c9a0889ee672cbe4728ae7766999f25c6d
aarati-K <aarati.agc10@gmail.com>
2021-05-20 16:44:01 -0500

    Small change

--- adding_counter.sh
+++ adding_counter.sh
@@ -3,12 +3,12 @@
 make
 for z in {0..5}
 do
-    mkdir -p adding_counter_zipf${z}
+    mkdir -p adding_counter_zipf${z}/adding_counter
     echo "zipf${z}"
     for i in {1..10}
     do
         taskset -c 39 ./benchmark.out workloads/zipf_${z}_chained_default_100M
-        cp output/zipf_${z}_chained_default_100M_adding_counter adding_counter_zipf${z}/${i}
+        cp output/zipf_${z}_chained_default_100M_adding_counter adding_counter_zipf${z}/adding_counter/${i}
         sleep 10
     done
     echo "--------"
