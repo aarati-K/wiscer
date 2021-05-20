8a9f569056d218cca2d577431264731e00cc1938
aarati-K <aarati.agc10@gmail.com>
2021-05-20 12:02:09 -0500

    Adding counter for zipf 1

--- adding_counter.sh
+++ adding_counter.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 make
-mkdir adding_counter_zipf0
+mkdir adding_counter_zipf1
 for i in {1..10}
 do
     echo ${i}
-    taskset -c 39 ./benchmark.out workloads/zipf_0_chained_default_100M
+    taskset -c 39 ./benchmark.out workloads/zipf_1_chained_default_100M
     echo "--------"
-    cp output/zipf_0_chained_default_100M_adding_counter adding_counter_zipf0/${i}
+    cp output/zipf_1_chained_default_100M_adding_counter adding_counter_zipf1/${i}
     sleep 10
 done
