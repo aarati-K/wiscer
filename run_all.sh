6a893d96651e519832f211adacc39f0bbd01736c
aarati-K <aarati.agc10@gmail.com>
2021-05-27 00:49:54 -0500

    Changes to steady state workload + small change to run script

--- run_all.sh
+++ run_all.sh
@@ -6,40 +6,52 @@ make clean
 # Default
 make default
 taskset -c 39 ./benchmark.out workloads/static_popularity_default_zipf0
+echo "-------------"
 sleep 10
 taskset -c 39 ./benchmark.out workloads/static_popularity_default_zipf1
+echo "-------------"
 sleep 10
 taskset -c 39 ./benchmark.out workloads/static_popularity_default_zipf2
+echo "-------------"
 sleep 10
 
 # Adaptive
 make adaptive
 taskset -c 39 ./benchmark.out workloads/static_popularity_adaptive_zipf0
+echo "-------------"
 sleep 10
 taskset -c 39 ./benchmark.out workloads/static_popularity_adaptive_zipf1
+echo "-------------"
 sleep 10
 taskset -c 39 ./benchmark.out workloads/static_popularity_adaptive_zipf2
+echo "-------------"
 sleep 10
 
 # POPULARITY CHURN
 make default
 taskset -c 39 ./benchmark.out workloads/medium_churn_default
+echo "-------------"
 sleep 10
 taskset -c 39 ./benchmark.out workloads/high_churn_default
+echo "-------------"
 sleep 10
 
 make adaptive
 taskset -c 39 ./benchmark.out workloads/medium_churn_adaptive
+echo "-------------"
 sleep 10
 taskset -c 39 ./benchmark.out workloads/high_churn_adaptive
+echo "-------------"
 sleep 10
 
 # STEADY STATE
 make default
 taskset -c 39 ./benchmark.out workloads/steady_state_default
+echo "-------------"
 sleep 10
 make adaptive
 taskset -c 39 ./benchmark.out workloads/steady_state_adaptive
+echo "-------------"
 sleep 10
 
 # READ MOSTLY
