bda4e2eaad8cab8e6736a58904c9fcc9343b54bf
aarati-K <aarati.agc10@gmail.com>
2021-09-13 23:26:25 -0500

    One more data point

--- learning_efficiency_test.sh
+++ learning_efficiency_test.sh
@@ -5,7 +5,7 @@ mkdir -p output
 mkdir -p learning_efficiency
 for z in {1,}
 do
-    for r in {0.125,0.25,0.5,1,2,4,8,16,32,64,128}
+    for r in {0.0625,0.125,0.25,0.5,1,2,4,8,16,32,64,128}
     do
         echo hms/hashmap_zipf_${z}_${r}n.out
         taskset -c 39 ./benchmark.out workloads/learning_efficiency_test_zipf_${z} 0 hms/hashmap_zipf_${z}_${r}n.out
