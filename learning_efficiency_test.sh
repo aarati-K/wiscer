351771bab6463648bbdef025dec6b1dfa640bcf5
aarati_K <aarati_K@c220g5-111232.wisc.cloudlab.us>
2021-09-13 21:52:27 -0500

    Another bug fix

--- learning_efficiency_test.sh
+++ learning_efficiency_test.sh
@@ -8,7 +8,7 @@ do
     for r in {0.5,1,2,4,8,16,32,64}
     do
         echo hms/hashmap_zipf_${z}_${r}n.out
-        taskset -c 39 ./benchmark.out workloads/learning_efficiency_test_zipf_{z} 0 hms/hashmap_zipf_${z}_${r}n.out
+        taskset -c 39 ./benchmark.out workloads/learning_efficiency_test_zipf_${z} 0 hms/hashmap_zipf_${z}_${r}n.out
         cp output/learning_efficiency_test learning_efficiency/zipf_${z}_${r}n
         echo "--------"
         sleep 10
