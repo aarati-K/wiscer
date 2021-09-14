eb0b5f92bca632589a2e7ef4e05dd7abb24f7c94
aarati-K <aarati.agc10@gmail.com>
2021-09-13 22:50:57 -0500

    Add more data points

--- learning_efficiency_learn.sh
+++ learning_efficiency_learn.sh
@@ -5,9 +5,9 @@
 make learning
 mkdir -p output
 mkdir -p hms
-for z in {1,2,3}
+for z in {1,}
 do
-    for r in {0.5,1,2,4,8,16,32,64}
+    for r in {0.125,0.25,0.5,1,2,4,8,16,32,64,128}
     do
         echo workloads/learning_efficiency_learn_zipf_${z}_${r}n
         taskset -c 39 ./benchmark.out workloads/learning_efficiency_learn_zipf_${z}_${r}n
