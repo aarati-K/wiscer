eac41b9117a18f124b0c1e7b8852ebbccfab97c1
aarati-K <aarati.agc10@gmail.com>
2021-10-12 02:31:45 -0500

    Clean-up code

--- store_workload.cpp
+++ store_workload.cpp
@@ -32,47 +32,29 @@ void StoreWorkload::free() {
     ops_file.close();
 }
 
-// Helper functions for sorting
-
-// To heapify a subtree rooted with node i which is
-// an index in arr[]. n is size of heap
-
-void StoreWorkload::_heapify(ulong arr[], long n, long i)
-{
+void StoreWorkload::_heapify(ulong arr[], long n, long i) {
     long largest = i; // Initialize largest as root
     long l = 2 * i + 1; // left = 2*i + 1
     long r = 2 * i + 2; // right = 2*i + 2
- 
-    // If left child is larger than root
+
     if (l < n && arr[l] > arr[largest])
         largest = l;
- 
-    // If right child is larger than largest so far
+
     if (r < n && arr[r] > arr[largest])
         largest = r;
- 
-    // If largest is not root
+
     if (largest != i) {
         swap(arr[i], arr[largest]);
- 
-        // Recursively heapify the affected sub-tree
         _heapify(arr, n, largest);
     }
 }
- 
-// main function to do heap sort
-void StoreWorkload::_heapSort(ulong arr[], long n)
-{
-    // Build heap (rearrange array)
+
+void StoreWorkload::_heapSort(ulong arr[], long n) {
     for (long i = n / 2 - 1; i >= 0; i--)
         _heapify(arr, n, i);
- 
-    // One by one extract an element from heap
+
     for (long i = n - 1; i > 0; i--) {
-        // Move current root to end
         swap(arr[0], arr[i]);
- 
-        // call max heapify on the reduced heap
         _heapify(arr, i, 0);
     }
 }
