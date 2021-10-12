e1b60ca97a62893ec4f9703b19222b37e5772d40
aarati-K <aarati.agc10@gmail.com>
2021-10-12 01:31:10 -0500

    Restructure run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -3,51 +3,59 @@
 make clean
 mkdir -p output
 
-# STATIC POPULARITY
-# Default
+echo "STATIC POPULARITY UNIFORM DISTRIBUTION"
 make default
 echo "workloads/static_popularity_default_zipf0"
 taskset -c 1 ./benchmark.out workloads/static_popularity_default_zipf0
 echo "-------------"
 sleep 10
-echo "workloads/static_popularity_default_zipf1"
-taskset -c 1 ./benchmark.out workloads/static_popularity_default_zipf1
-echo "-------------"
-sleep 10
-
-# Adaptive
 make adaptive
 echo "workloads/static_popularity_adaptive_zipf0"
 taskset -c 1 ./benchmark.out workloads/static_popularity_adaptive_zipf0
 echo "-------------"
 sleep 10
+echo ""
+
+echo "STATIC POPULARITY LOW SKEW"
+make default
+echo "workloads/static_popularity_default_zipf1"
+taskset -c 1 ./benchmark.out workloads/static_popularity_default_zipf1
+echo "-------------"
+sleep 10
+make adaptive
 echo "workloads/static_popularity_adaptive_zipf1"
 taskset -c 1 ./benchmark.out workloads/static_popularity_adaptive_zipf1
 echo "-------------"
 sleep 10
+echo ""
 
-# POPULARITY CHURN
+echo "MEDIUM POPULARITY CHURN"
 make default
 echo "workloads/medium_churn_default"
 taskset -c 1 ./benchmark.out workloads/medium_churn_default
 echo "-------------"
 sleep 10
-echo "workloads/high_churn_default"
-taskset -c 1 ./benchmark.out workloads/high_churn_default
-echo "-------------"
-sleep 10
-
 make adaptive
 echo "workloads/medium_churn_adaptive"
 taskset -c 1 ./benchmark.out workloads/medium_churn_adaptive
 echo "-------------"
 sleep 10
+echo ""
+
+echo "HIGH POPULARITY CHURN"
+make default
+echo "workloads/high_churn_default"
+taskset -c 1 ./benchmark.out workloads/high_churn_default
+echo "-------------"
+sleep 10
+make adaptive
 echo "workloads/high_churn_adaptive"
 taskset -c 1 ./benchmark.out workloads/high_churn_adaptive
 echo "-------------"
 sleep 10
+echo ""
 
-# STEADY STATE
+echo "STEADY STATE"
 make default
 echo "workloads/steady_state_default"
 taskset -c 1 ./benchmark.out workloads/steady_state_default
@@ -58,8 +66,9 @@ echo "workloads/steady_state_adaptive"
 taskset -c 1 ./benchmark.out workloads/steady_state_adaptive
 echo "-------------"
 sleep 10
+echo ""
 
-# READ MOSTLY
+echo "READ MOSTLY"
 make default
 echo "workloads/read_mostly_default"
 taskset -c 1 ./benchmark.out workloads/read_mostly_default
@@ -70,4 +79,4 @@ echo "workloads/read_mostly_adaptive"
 taskset -c 1 ./benchmark.out workloads/read_mostly_adaptive
 echo "-------------"
 sleep 10
-
+echo ""
